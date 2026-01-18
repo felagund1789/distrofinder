56bfb1ed1a8056494dad2479cb8e81f4bac36f38
Konstantinos K <raistlindragonl@gmail.com>
2026-01-18 12:33:44 +0200

    Remove unused distros data from Home component and update DistroGrid usage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import DistroGrid from "../components/distro/DistroGrid";
 import AppLayout from "../components/layout/AppLayout";
-import { useDistros } from "../context/DistroContext";
 import DistroDetail from "./DistroDetail";
 
 export default function Home() {
-  const { distros } = useDistros();
-
   return (
     <AppLayout>
       <Routes>
-        <Route path="/" element={<DistroGrid distros={distros} />} />
+        <Route path="/" element={<DistroGrid />} />
         <Route path="/d/:slug" element={<DistroDetail />} />
       </Routes>
     </AppLayout>
