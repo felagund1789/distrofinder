92fd564294afff886362c714ef9af44b6bfa0d37
felagund1789 <raistlindragonl@gmail.com>
2026-01-20 11:28:10 +0200

    Add scroll-to-top on route change

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,10 +2,12 @@ import { Route, Routes } from "react-router-dom";
 import DistroGrid from "../components/distro/DistroGrid";
 import AppLayout from "../components/layout/AppLayout";
 import DistroDetail from "./DistroDetail";
+import { ScrollToTop } from "../components/routing/ScrollToTop";
 
 export default function Home() {
   return (
     <AppLayout>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<DistroGrid />} />
         <Route path="/d/:slug" element={<DistroDetail />} />
