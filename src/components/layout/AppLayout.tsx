7bf0f8e9a20da97fb61afd46fae5190843ac62d2
Konstantinos K <raistlindragonl@gmail.com>
2026-01-25 13:24:41 +0200

    feat: make header clickable to navigate to home

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,9 +1,12 @@
 import type { ReactNode } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function AppLayout({ children }: { children: ReactNode }) {
+  const navigate = useNavigate();
+
   return (
     <div className="layout">
-      <header className="topnav">DistroFinder</header>
+    <header className="topnav" onClick={() => navigate("/")}>DistroFinder</header>
       <main>{children}</main>
     </div>
   );
